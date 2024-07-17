64b30a2712097289f5f6c4bd8d684599ac4afb9c
MBAir 2017 <mbair2017@192.168.2.5>
2024-07-17 01:21:14 -0700

    sii

--- src/components/auctions/AuctionBody.jsx
+++ src/components/auctions/AuctionBody.jsx
@@ -18,7 +18,7 @@ export const AuctionBody = () => {
 
       {stateLogout ? (
         <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 p-5 g-3 border mt-1 ">
-          {items.map((doc, i) => {
+          {items.filter(el => el.sucursal === 'San Carlos').map((doc, i) => {
             return <AuctionCard item={doc} key={i} />;
           })}
         </div>
