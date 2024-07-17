d32e74731db4feaec946d827b6cbf96cad1970f2
MBAir 2017 <mbair2017@192.168.2.5>
2024-07-16 23:28:21 -0700

    first

---
+++ src/components/auctions/AuctionBody.jsx
@@ -0,0 +1,30 @@
+import { useContext } from 'react';
+
+import { AddAuction } from './AddAuction';
+import { AuctionCard } from './AuctionCard';
+
+import { FireStoreDataContext } from '../../context/FireStoreDataContext';
+import { AuthContext } from '../../context/AuthContext';
+
+export const AuctionBody = () => {
+  const { items } = useContext(FireStoreDataContext);
+  const { stateLogout } = useContext(AuthContext);
+
+  console.log(items);
+
+  return (
+    <div className="">
+      {localStorage.getItem('userEmailLS') !== null && <AddAuction />}
+
+      {stateLogout ? (
+        <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 p-5 g-3 border mt-1 ">
+          {items.map((doc, i) => {
+            return <AuctionCard item={doc} key={i} />;
+          })}
+        </div>
+      ) : (
+        ''
+      )}
+    </div>
+  );
+};
---
+++ src/components/auctions/AuctionCard.jsx
@@ -0,0 +1,76 @@
+import { useContext } from 'react';
+import { FireStoreDataContext } from '../../context/FireStoreDataContext';
+  import './auctionCard.css'
+
+
+
+
+
+
+
+
+export const AuctionCard = ({ item }) => {
+  const formateador = new Intl.DateTimeFormat("es-MX", { dateStyle: 'long', timeStyle: 'short' });
+   
+  const milisegundosComoFecha = (milisegundos) => {
+      return formateador.format(new Date(milisegundos));
+  }; 
+
+
+
+  const { deleteById, setToggle, toggle } = useContext(FireStoreDataContext);
+
+  return (
+    <div className="card shadow-sm ">
+      <div
+        style={{
+          height: '180px',
+          backgroundImage: `url(${item.imgUrl})`,
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+        }}
+        className="w-100 mt-4"
+      />
+
+      <div className="card-body p-4 ">
+
+        {/*<p>Creado: {  new Date(item.duration).toLocaleDateString("es-ES", {year: 'numeric', month: 'long', day: 'numeric'})}</p>*/}
+        <p> {milisegundosComoFecha(item.duration)}  </p>
+        <hr />
+        <p>Nombre: <span>{item.name}</span></p>
+        <p>para: <span>{item.para}</span></p>
+        <p> categoria: <span>{item.category}</span></p>
+        <p> precio: <span>{item.price}</span></p>
+        <hr />
+
+        <p> marca: <span>{item.marca}</span></p>
+        <p> talla: <span>{item.talla}</span></p>
+        <p> color: <span>{item.color}</span></p>
+        <p> tela: <span>{item.tela}</span></p>
+        <p> Stock San Carlos: <span>{item.stockSanCarlos}</span></p>
+        <hr />
+
+        <p> Descripcion <span>{item.description}</span></p>
+
+
+        <hr/>
+
+
+        <div className='btnBorrarInforme'> 
+          <button
+            className="btn btn-danger"
+            onClick={() => {
+              if (window.confirm(`Quiere Borrar este Documento? ${item.name}`)) {
+                  deleteById(item.id, item.imgName);
+                  setToggle(!toggle);
+                }
+              }}
+        >
+          Borrar
+        </button>
+        </div>
+      </div>
+    </div>
+  );
+};
