9c8c924a71decd7c09a5f53e8834191d29d25247
fer <fer@fers-iMac.local>
2024-08-07 09:50:00 -0700

    lplp

--- src/components/auctions/AuctionCard.jsx
+++ src/components/auctions/AuctionCard.jsx
@@ -27,7 +27,7 @@ export const AuctionCard = ({ item }) => {
       let typeNumber = 4;
       let errorCorrectionLevel = 'L';
       let qr = qrcode(typeNumber, errorCorrectionLevel);
-      qr.addData(`https://xiperafa.github.io/polancoEcommerc/item/${item.id}`);
+      qr.addData(`https://xiperafa.github.io/adminPolanco/${item.id}`);
       qr.make();
       return <div dangerouslySetInnerHTML={{ __html: qr.createImgTag() }} />
 }
