3849d944cb2b2a5205e665a48d2389ea8bbce768
fer <fer@fers-iMac.local>
2024-08-15 16:37:49 -0700

    lplp

--- src/components/auctions/AuctionBody.jsx
+++ src/components/auctions/AuctionBody.jsx
@@ -10,7 +10,7 @@ export const AuctionBody = () => {
   const { items } = useContext(FireStoreDataContext);
   const { stateLogout } = useContext(AuthContext);
 
-  console.log(items);
+  console.log(items.filter(el => el.sucursal === 'San Carlos'));
 
   return (
     <div className="">
@@ -18,7 +18,7 @@ export const AuctionBody = () => {
 
       {stateLogout ? (
         <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 p-5 g-3 border mt-1 ">
-          {items.sort((a, b) => b.duration - a.duration).filter(el => el.sucursal === 'San Carlos').map((doc, i) => {
+          {items.filter(el => el.sucursal === 'San Carlos').sort((a, b) => b.duration - a.duration).slice(0,12).map((doc, i) => {
             return <AuctionCard item={doc} key={i} />;
           })}
         </div>
